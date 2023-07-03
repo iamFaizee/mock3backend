bae30ea28f94d2e6a2dbc82437ef5fefbf894275
iamFaizee <khanfaizan1123@gmail.com>
2023-07-03 21:25:20 +0530

    Backend done

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/mock3db")
-// const connection = mongoose.connect(process.env.MONGO_CONNECTION)
+// const connection = mongoose.connect("mongodb://127.0.0.1:27017/mock3db")
+const connection = mongoose.connect(process.env.MONGO_CODE)
 .then((res) => {
     console.log('connection established')
 })
